bd3dc420d0ff30f3460f8292488150b08a4a325f
Anirudha Paul <anirudhaprasun@gmail.com>
2022-12-14 23:31:55 -0500

    Adding comments for the hidden hitbox

--- triage/generators/HiddenRectangleHitboxGenerator.java
+++ triage/generators/HiddenRectangleHitboxGenerator.java
@@ -16,14 +16,17 @@ public class HiddenRectangleHitboxGenerator {
 
     public GameObject generate(Vec2d positionInWorld, Vec2d size) {
 
-        // Because the origin point is (0,0) and end corner point is (width, height)
         GameObject hiddenRectangleHitboxObject = new GameObject(
                 GameObjectId.HIDDEN_RECTANGLE_HITBOX.toString(),
                 new TransformComponent(positionInWorld, size));
 
+        // Keeping this just for visualization while debugging
+        // TODO: remove it in the final version
         DrawableRectangleComponent drawableComponent = new DrawableRectangleComponent(hiddenRectangleHitboxObject);
         hiddenRectangleHitboxObject.setDefaultColor(Color.RED);
 
+        // Hitboxes are a part of the collisionComponent
+        // Collision behaviour static means it won't move after collision
         AABHitbox hitbox = new AABHitbox(positionInWorld, size);
         CollisionComponent collisionComponent = new CollisionComponent(hiddenRectangleHitboxObject, hitbox);
         collisionComponent.setCollisionBehaviour(CollisionBehaviour.STATIC);
